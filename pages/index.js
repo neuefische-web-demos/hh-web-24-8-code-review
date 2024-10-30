1cac5a4c14c608c2f6342ece224977bb3241f2da
Jessica Lörs <54777853+JessicaLoers@users.noreply.github.com>
2024-10-30 15:06:17 +0100

    Update pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-import { Btn } from "@/components/Button";
+import  Button from "@/components/Button";
 
 
 import Title from "@/components/Title/Title.js";
